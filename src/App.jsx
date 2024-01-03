f75d59915f4191fcee8744dc598731ebacd3a010
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-01-03 18:52:57 +0700

    feat: setup vite react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
+const App = () => {
+  const router = (
+    {
+    }
+  )
+
+  return (
+    <>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
