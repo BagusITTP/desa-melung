21714fac6f4b048d6d64d9025280ab7c25913393
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-01-05 15:18:23 +0700

    feat: add constants

---
+++ src/constants/titleMap.js
@@ -0,0 +1,18 @@
+const titleMap = {
+  '/admin/dashboard': 'Dashboard',
+  '/admin/objek-wisata': 'Daftar Objek Wisata',
+  '/admin/harga/tiket-masuk': 'Daftar Harga Tiket Masuk',
+  '/admin/harga/objek-wisata': 'Daftar Harga Objek Wisata',
+  "/admin/objek-wisata/tambah": "Tambah Objek Wisata",
+  '/admin/harga/kendaraan': 'Daftar Harga Kendaraan',
+  '/admin/harga/program': 'Daftar Harga Program',
+  '/admin/harga/sewa-pendopo': 'Daftar Harga Sewa Pendopo',
+  '/admin/penilaian': 'Daftar Penilaian',
+  '/admin/pesanan/tiket-masuk': 'Daftar Pesanan Tiket Masuk',
+  '/admin/pesanan/objek-wisata': 'Daftar Pesanan Objek Wisata',
+  '/admin/pesanan/program': 'Daftar Pesanan Program',
+  '/admin/pesanan/sewa-pendopo': 'Daftar Pesanan Sewa Pendopo',
+  '/admin/akun': 'Daftar Akun'
+};
+
+export default titleMap;
