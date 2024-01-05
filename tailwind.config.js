06d8becb0e5555cd1f22b3fe03dcf6c87e3e7d62
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-01-05 14:05:20 +0700

    feat: add setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: '#258752',
+          "Medium-Dark": "#1A5F39",
+          "Red": '#EE0700',
+          "Yellow": '#FFDE7D',
+          "Green": '#038674',
+        },
+        secondary: {
+          DEFAULT: '#8A8A8A',
+          "Dark": "#151515",
+          "Medium-Dark": "#151515",
+          "Medium-Light": "#D0D0D0"
+        }
+      }
+    },
   },
   plugins: [],
 }
