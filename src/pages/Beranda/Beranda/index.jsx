f2b18094570e3a2654fb4420256f076fc5385fc4
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-02-04 11:47:41 +0700

    fix(beranda): resolve get data in page beranda

--- src/pages/Beranda/Beranda/index.jsx
+++ src/pages/Beranda/Beranda/index.jsx
@@ -1,11 +1,14 @@
-import { Panel, Col, FlexboxGrid } from 'rsuite';
+import { Panel, Col, FlexboxGrid, Placeholder } from 'rsuite';
 import Savannah from '../../../assets/Savannah.svg';
 import Book from '../../../assets/Book.svg';
 import People from '../../../assets/People.svg';
 import PaketWisata from '../../../assets/images/paket-wisata.webp';
 import PagubuganMelung from '../../../assets/images/pagubugan-melung.webp';
-import ImageList from '../../../constants/ImageList';
+import { BiChevronRight } from "react-icons/bi";
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { articleSelector, getArticle } from '../../../store/articleSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CardList = [
   {
@@ -39,25 +42,29 @@ const Card = ({ data }) => (
 );
 
 const Index = () => {
-  const chunkSize = 3;
+  const [defaultData, setDefaultData] = useState([])
+  const dispatch = useDispatch()
+  const { status } = useSelector(state => state.articleSlice)
+  const tourPackage = useSelector(articleSelector.selectAll)
 
-  const imageChunks = [];
+  useEffect(() => {
+    dispatch(getArticle())
+  }, [dispatch])
 
-  for (let i = 0; i < ImageList.length; i += chunkSize) {
-    const chunk = ImageList.slice(i, i + chunkSize);
-    imageChunks.push(chunk);
-  }
+  useEffect(() => {
+    setDefaultData(tourPackage != [] ? tourPackage : [])
+  }, [tourPackage])
   return (
     <>
       <div className={`w-full bg-[url('/jumbotron.webp')] h-[calc(100vh-4rem)] bg-no-repeat bg-cover bg-bottom`}>
-        <div className="flex flex-col gap-3 justify-center items-center w-full h-full backdrop-blur-sm">
+        <div className="flex flex-col gap-3 justify-center items-center w-full h-full backdrop-opacity-10 backdrop-invert bg-black/30">
           <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold text-center ">Desa Wisata Melung</h1>
           <p className="text-white text-lg md:text-xl w-3/4 md:w-1/2 text-center">Manfaatkan liburan kamu untuk melepaskan semua beban agar pikiran dan tubuh menjadi sehat.</p>
         </div>
       </div>
       <section id="serve" className="flex justify-center py-8">
         <div className="container flex flex-col gap-5">
-          <h4 className="text-center text-2xl">Apa yang disuguhkan dari Desa Wisata Melung?</h4>
+          <h4 className="text-center text-xl lg:text-2xl xl:text-2xl">Apa yang disuguhkan dari Desa Wisata Melung?</h4>
           <FlexboxGrid className="gap-y-2">
             {
               CardList.map((card, index) => (
@@ -69,7 +76,7 @@ const Index = () => {
       </section>
       <section id="order" className="flex justify-center py-8 bg-secondary-Light">
         <div className="container flex flex-col gap-5 h-full md:h-auto">
-          <h4 className="text-center text-2xl">Apa yang ditawarkan oleh Desa Wisata Melung?</h4>
+          <h4 className="text-center text-xl lg:text-2xl xl:text-2xl">Apa yang ditawarkan oleh Desa Wisata Melung?</h4>
           <FlexboxGrid justify='space-around' className="gap-y-2">
             <FlexboxGrid.Item as={Col} colspan={24} xs={24} sm={24} md={12} lg={12} xl={12}>
               <img className="max-w-full rounded-2xl" src={PagubuganMelung} alt="sawah" />
@@ -98,22 +105,35 @@ const Index = () => {
           </FlexboxGrid>
         </div>
       </section>
-      <section id="gallery" className="flex justify-center py-8">
-        <div className="container flex flex-col gap-5">
-          <h4 className="text-center text-2xl">Pemandangan Indah dan Serunya Belajar di Desa Wisata Melung</h4>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {
-              imageChunks.map((chunk, index) => (
-                <div key={index} className="grid gap-4">
-                  {
-                    chunk.map((image, index) => (
-                      <img key={index} loading='lazy' className="h-full max-w-full rounded-lg" src={image} alt="images" />
-                    ))
-                  }
-                </div>
-              ))
-            }
+      <section id="news" className="flex justify-center py-8">
+        <div className="container flex flex-col gap-4">
+          <div className="flex justify-between w-full">
+            <h4 className="text-center text-xl lg:text-2xl xl:text-2xl">Berita Terkini</h4>
+            <Link className="flex items-center gap-1 text-primary hover:text-primary-Medium-Dark !no-underline hover:!no-underline">
+              <p className="text-sm">Lihat Semua</p>
+              <BiChevronRight size={20} />
+            </Link>
           </div>
+          <FlexboxGrid className="gap-y-4">
+            {
+              status === "success"
+                ?
+                defaultData?.slice(0, 8).map((data, index) =>
+                (
+                  <FlexboxGrid.Item as={Col} colspan={24} xs={24} sm={24} md={8} lg={8} xl={6} className="!overflow-hidden !rounded-md" key={index}>
+                    <Link to={`/berita/${data.id}`} className="w-full h-full !no-underline hover:!no-underline !rounded-md !overflow-hidden group">
+                      <img className="!h-full !w-full sm:h-full xl:h-full max-w-full max-h-52 !rounded-md transition ease-in-out delay-150 bg-blue-500 group-hover:-translate-y-1 group-hover:scale-110 group-hover:bg-indigo-500 duration-300 object-cover" src={data?.article_images?.[0]?.url} alt="paket" />
+                      <div className="flex flex-col gap-1 w-full pt-2">
+                        <h6 className="text-md font-bold group-hover:text-primary">{data?.title}</h6>
+                        <p className="text-sm text-secondary">{data?.createAt}</p>
+                      </div>
+                    </Link>
+                  </FlexboxGrid.Item>
+                ))
+                :
+                <Placeholder.Paragraph rows={5} graph="image" />
+            }
+          </FlexboxGrid>
         </div>
       </section>
     </>
