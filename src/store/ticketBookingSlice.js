2f92e4e1d7919588f2b1a698a2557863e214e4e8
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-02-13 22:28:56 +0700

    feat(ticketBooking): add page riwayat tiket masuk and ticketBookingSlice

--- src/store/ticketBookingSlice.js
+++ src/store/ticketBookingSlice.js
@@ -19,13 +19,58 @@ export const getTicketBooking = createAsyncThunk("ticketBooking/getTicketBooking
   return json
 })
 
+export const getUserTicketBooking = createAsyncThunk("ticketBooking/getUserTicketBooking", async () => {
+  const cookies = new Cookies()
+  let token = cookies.get("token")
+
+  const response = await fetch(`${ticketBookingAPI}/user`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  })
+  const json = await response.json()
+
+  return json
+})
+
+export const getOrderIdTicket = createAsyncThunk("ticketBooking/getOrderIdTicket", async (id) => {
+  const cookies = new Cookies()
+  let token = cookies.get("token")
+
+  const response = await fetch(`${ticketBookingAPI}/order?order_id=${id}`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  })
+  const json = await response.json()
+
+  return json
+})
+
+export const getPaymentTicket = createAsyncThunk("ticketBooking/getPaymentTicket", async (data) => {
+  const cookies = new Cookies()
+  let token = cookies.get("token")
+  const response = await axios.post(`${ticketBookingAPI}/payment/success`, data,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: data
+    })
+
+  return response
+})
+
 export const setTicketBooking = createAsyncThunk("ticketBooking/setTicketBooking", async (data) => {
   const cookies = new Cookies()
   let token = cookies.get("token")
   const response = await axios.post(ticketBookingAPI, data,
     {
       headers: {
-        "Content-Type": "multipart/form-data",
+        "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
       },
       body: data
@@ -69,6 +114,29 @@ const ticketBookingSlice = createSlice({
       })
       .addCase(getTicketBooking.rejected, (state) => {
         state.status = "failed"
+      })
+      .addCase(getUserTicketBooking.fulfilled, (state, action) => {
+        state.status = "success",
+          ticketBookingEntity.setAll(state, action.payload.data)
+      })
+      .addCase(getUserTicketBooking.pending, (state) => {
+        state.status = "pending"
+      })
+      .addCase(getUserTicketBooking.rejected, (state) => {
+        state.status = "failed"
+      })
+      .addCase(getOrderIdTicket.fulfilled, (state, action) => {
+        state.status = "success",
+          ticketBookingEntity.setAll(state, action.payload.data)
+      })
+      .addCase(getOrderIdTicket.pending, (state) => {
+        state.status = "pending"
+      })
+      .addCase(getOrderIdTicket.rejected, (state) => {
+        state.status = "failed"
+      })
+      .addCase(getPaymentTicket.fulfilled, (state, action) => {
+        ticketBookingEntity.addOne(state, action.payload)
       })
       .addCase(setTicketBooking.fulfilled, (state, action) => {
         ticketBookingEntity.addOne(state, action.payload)
